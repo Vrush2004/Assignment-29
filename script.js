498984beb4314af7da45b68385ce787da45e5d4f
Vrush2004 <vrushalichaube@gmail.com>
2024-10-14 20:56:59 +0530

    text operation

---
+++ script.js
@@ -0,0 +1,18 @@
+inputElement = document.getElementById("input");
+outputElement = document.getElementById("output");
+
+function handleUpperCase(){
+    outputElement.value = inputElement.value.toUpperCase();
+}
+
+function handleLowerCase(){
+    outputElement.value = inputElement.value.toLowerCase();
+}
+
+function handleWordCount(){
+    
+}
+
+function handleCharacterCount(){
+    
+}
